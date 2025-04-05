f0df85fc90be5362cad84e05111aac5b507a530b
Supriya9876 <supriyabatabyal346@gmail.com>
2025-04-05 15:32:15 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/helloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+	
+	@RequestMapping("/simple")
+	public String simple() {
+		return "Hello World";
+	}
+
+}
